0c52965c88492c977198eb6eaff940e3b5b8bf9c
NataliaOrlova <natakarelskaya@yandex.ru>
2023-04-06 19:15:00 +0400

    chanched revert function

--- src/homework2/Reverse.java
+++ src/homework2/Reverse.java
@@ -5,14 +5,22 @@ package homework2;
 public class Reverse {
     Node head;
     Node tail;
-    private void revert(Node currentNode, Node previousNode) {
-        if (currentNode.next == null) {
-            head = currentNode;
-        } else {
-            revert (currentNode.next, currentNode);
-        }
-        currentNode.next = previousNode;
 
+    public void revert(){
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous; // меняем их местами
+            currentNode.previous = next;
+            if (previous == null) { // если находимся в head
+                tail = currentNode; // меняем ее на tail
+            }
+            if (next == null) { // если это последний элемент
+                head = currentNode; // меняем его на head
+            }
+            currentNode = next;
+        }
     }
 
     public class Node {
